ced046533e189856065eaeae557cc2c28abdfc6b
Lukas <lukas.almgren03@gmail.com>
2023-01-18 15:46:13 +0100

    Quads utplacerade på varje gridspace och har textures

--- Assets/MeshBuilder.cs
+++ Assets/MeshBuilder.cs
@@ -12,6 +12,14 @@ namespace Scenes
         
         public Matrix4x4 VertexMatrix = Matrix4x4.identity;
         public Matrix4x4 TextureMatrix = Matrix4x4.identity;
+        
+        private Vector2[] stateTextureUV =
+        {
+            new Vector2(0, 1), new Vector2(0.5f, 0.5f),
+            new Vector2(0.5f, 0.5f), new Vector2(1, 0),
+            new Vector2(0.5f, 1), new Vector2(1, 0.5f),
+            new Vector2(0, 0.5f), new Vector2(0.5f, 0)
+        };
 
         public int AddVertex(Vector3 position, Vector3 normal, Vector2 uv)
         {
@@ -33,6 +41,33 @@ namespace Scenes
             triangles.Add(bottomLeft);
             triangles.Add(topRight);
             triangles.Add(bottomRight);
+        }
+        
+        //add 2 vectors for minimum and maximum uv
+        public void CreateQuad(Vector3 botLeft, Vector3 topLeft, Vector3 topRight, Vector3 botRight, Vector3 normal, int state)
+        {
+            Vector2 min = stateTextureUV[state * 2];
+            Vector2 max = stateTextureUV[state * 2 + 1];
+
+
+                int a = AddVertex(
+                botLeft, 
+                normal, 
+                new Vector2(0, 0));
+            int b = AddVertex(
+                topLeft, 
+                normal, 
+                new Vector2(0, 1));
+            int c = AddVertex(
+                topRight, 
+                normal, 
+                new Vector2(1, 1));
+            int d = AddVertex(
+                botRight, 
+                normal, 
+                new Vector2(1, 0));
+            
+            AddQuad(a, b, c, d);
         }
 
         public void Build(Mesh mesh)
--- Assets/Piece.cs
+++ Assets/Piece.cs
@@ -1,11 +1,39 @@
+using System;
 using Grid;
+using Scenes;
 using UnityEngine;
 
-[RequireComponent(typeof(GridTile))]
-public class Piece : MonoBehaviour 
+[ExecuteAlways]
+[RequireComponent(typeof(GridTile), typeof(MeshFilter), typeof(MeshRenderer))]
+public class Piece : MonoBehaviour
 {
+    private Mesh mesh;
+    private GridTile tile;
+    private state currentState;
+    
+    private void Start()
+    {
+        tile = GetComponent<GridTile>();
+        var meshFilter = GetComponent<MeshFilter>();
+        mesh = new Mesh { name = "Piece" };
+        meshFilter.sharedMesh = mesh;
+    }
+
+    private void Update()
+    {
+        MeshBuilder builder = new MeshBuilder();
+        
+        
+        
+        
+        builder.CreateQuad(new Vector3(-0.5f, 0, -0.5f),new Vector3(-0.5f, 0, 0.5f), new Vector3(0.5f, 0, 0.5f),
+            new Vector3(0.5f, 0, -0.5f), new Vector3(0, 1, 0), );
+        
+        builder.Build(mesh);
+    }
+
     private void OnDrawGizmos() {
-        var tile = GetComponent<GridTile>();
+        
         if (tile.GetProperty(GridTileProperty.Solid)) {
             Gizmos.color = Color.red;
         } else {
@@ -18,4 +46,14 @@ public class Piece : MonoBehaviour
         
         Gizmos.DrawCube(transform.position, new Vector3(1, 0.1f, 1));
     }
+
+    enum state
+    {
+        Grass,
+        Solid,
+        Water,
+        Bridge
+    }
+    
+
 }
